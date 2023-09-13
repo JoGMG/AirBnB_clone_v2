1ba372ba0ff5a9f21671070eed698fd9ccb80c75
JoGMG <joattah3@gmail.com>
2023-09-13 04:38:11 +0100

    deploy archive file

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,12 +4,36 @@ A python script that distributes a .tgz
 archive using Fabric.
 """
 import os
+from datetime import datetime
 from fabric.api import local, env, put, run
 
 """ Remote webservers detail to execute script """
 env.hosts = ["34.202.159.210", "54.90.4.252"]
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/alx_sevkey"
+
+
+def do_pack():
+    """ Generates a .tgz archive from the contents of web_static folder. """
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+        return output
+    except Exception:
+        return None
 
 
 def do_deploy(archive_path):
