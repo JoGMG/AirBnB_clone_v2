642c774afa5f0654740233cb206dba43715bcfb1
JoGMG <joattah3@gmail.com>
2023-09-14 19:06:35 +0100

    task 0 but w/puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,72 +1,78 @@
 # AirBnB clone web server setup and configuration
 
-exec { 'update':
-  provider => 'shell',
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+package { 'nginx':
+  ensure => 'installed',
 }
 
-exec { 'install Nginx':
-  provider => 'shell',
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['create directories'],
+file { 'create ../test':
+  ensure => directory,
+  path   => '/data/web_static/releases/test',
+  before => file['create ../index.html'],
 }
 
-exec { 'create directories':
-  provider => 'shell',
-  command  => 'sudo mkdir -p /data/web_static/releases/test /data/web_static/shared',
-  before   => file['create test html file'],
+file { 'create ../shared':
+  ensure => directory,
+  path   => '/data/web_static/shared',
 }
 
-file { 'create test html file':
+file { 'create ../index.html':
   ensure  => present,
   path    => '/data/web_static/releases/test/index.html',
-  content => '<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>',
-  before  => file['write into nginx config file'],
+  content => '<html>
+	<head>
+	</head>
+	<body>
+		Holberton School
+	</body>
+</html>',
+  before  => file['link ../test to ../current'],
 }
 
-exec { 'remove current directory, if exists':
-  provider => 'shell',
-  command  => '[ -d /data/web_static/current ] && sudo rm -rf /data/web_static/current',
-  before   => Exec['link test directory to current directory'],
-}
-
-exec { 'link test directory to current directory':
-  provider => 'shell',
-  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  before   => file['give user and group ownership'],
-}
-
-file { 'give user and group ownership':
-  ensure  => directory,
-  path    => '/data',
+file { 'remove ../current, if exists':
+  ensure  => absent,
+  path    => '/data/web_static/current',
   recurse => true,
+  before  => file['link ../test to ../current'],
+}
+
+file { 'link ../test to ../current':
+  ensure => 'symlink',
+  path   => '/data/web_static/releases/test/',
+  target => '/data/web_static/current',
+  force  => true,
+  before => file['give ownership'],
+}
+
+file { 'give ownership':
+  ensure  => directory,
+  path    => '/data/',
   user    => 'ubuntu',
   group   => 'ubuntu',
-  before  => file['write into nginx config file'],
+  recurse => true,
+  before  => file['nginx config file'],
 }
 
 file { 'create index.html':
   ensure  => present,
   path    => '/var/www/html/index.html',
   content => 'Hello, World!',
-  before  => file['write into nginx config file'],
+  before  => file['nginx config file'],
 }
 
-exec { 'create error directory':
-  provider => 'shell',
-  command  => 'sudo mkdir -p /var/www/error/',
-  before   => file['create 404.html'],
+file { 'create ../error':
+  ensure => directory,
+  path   => '/var/www/error/',
+  before => file['create 404.html'],
 }
 
 file { 'create 404.html':
   ensure  => present,
   path    => '/var/www/error/404.html',
   content => "Ceci n'est pas une page",
-  before  => file['write into nginx config file'],
+  before  => file['nginx config file'],
 }
 
-file { 'write into nginx config file':
+file { 'nginx config file':
   ensure  => present,
   path    => '/etc/nginx/sites-available/default',
   content => "server {
@@ -94,10 +100,10 @@ file { 'write into nginx config file':
     internal;
   }
 }",
-  before  => Exec['restart Nginx'],
+  before  => Exec['restart_nginx'],
 }
 
-exec { 'restart Nginx':
-  provider => 'shell',
-  command  => 'sudo service nginx restart',
+exec { 'restart_nginx':
+  command     => '/etc/init.d/nginx restart',
+  refreshonly => true,
 }
