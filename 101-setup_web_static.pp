27c0e1c661bef014b4a71ea6307c9a3bf575c1cb
JoGMG <joattah3@gmail.com>
2023-09-14 19:35:54 +0100

    checker passed

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -4,22 +4,28 @@ package { 'nginx':
   ensure => 'installed',
 }
 
-file { 'create ../test':
-  ensure  => directory,
-  path    => '/data/web_static/releases/test',
-  recurse => true,
-  before  => file['create ../index.html'],
+file { '/data':
+  ensure => directory,
 }
 
-file { 'create ../shared':
-  ensure  => directory,
-  path    => '/data/web_static/shared',
-  recurse => true,
+file { '/data/web_static':
+  ensure => directory,
 }
 
-file { 'create ../index.html':
+file { '/data/web_static/releases':
+  ensure => directory,
+}
+
+file { '/data/web_static/releases/test':
+  ensure => directory,
+}
+
+file { '/data/web_static/shared':
+  ensure => directory,
+}
+
+file { '/data/web_static/releases/test/index.html':
   ensure  => present,
-  path    => '/data/web_static/releases/test/index.html',
   content => '<html>
 	<head>
 	</head>
@@ -27,57 +33,43 @@ file { 'create ../index.html':
 		Holberton School
 	</body>
 </html>',
-  before  => file['link ../test to ../current'],
 }
 
-file { 'remove ../current, if exists':
+file { '/data/web_static/current':
   ensure  => absent,
-  path    => '/data/web_static/current',
   recurse => true,
-  before  => file['link ../test to ../current'],
 }
 
-file { 'link ../test to ../current':
+file { '/data/web_static/current':
   ensure => 'symlink',
-  path   => '/data/web_static/releases/test/',
-  target => '/data/web_static/current',
+  target => '/data/web_static/releases/test/',
   force  => true,
-  before => file['give ownership'],
 }
 
-file { 'give ownership':
+file { '/data/':
   ensure  => directory,
-  path    => '/data/',
   user    => 'ubuntu',
   group   => 'ubuntu',
   recurse => true,
-  before  => file['nginx config file'],
 }
 
-file { 'create index.html':
+file { '/var/www/html/index.html':
   ensure  => present,
-  path    => '/var/www/html/index.html',
   content => 'Hello, World!',
-  before  => file['nginx config file'],
 }
 
-file { 'create ../error':
+file { '/var/www/error/':
   ensure  => directory,
-  path    => '/var/www/error/',
   recurse => true,
-  before  => file['create 404.html'],
 }
 
-file { 'create 404.html':
+file { '/var/www/error/404.html':
   ensure  => present,
-  path    => '/var/www/error/404.html',
   content => "Ceci n'est pas une page",
-  before  => file['nginx config file'],
 }
 
-file { 'nginx config file':
+file { '/etc/nginx/sites-available/default':
   ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
   content => "server {
   listen 80;
   listen [::]:80 default_server;
@@ -103,10 +95,8 @@ file { 'nginx config file':
     internal;
   }
 }",
-  before  => Exec['restart_nginx'],
 }
 
-exec { 'restart_nginx':
-  command     => '/etc/init.d/nginx restart',
-  refreshonly => true,
+exec { 'nginx restart':
+  path => '/etc/init.d/',
 }
