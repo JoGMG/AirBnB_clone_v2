c7ab94dfe2a7001f96c860ec787d2eab1507ed75
Joshua Attah <112636188+JoGMG@users.noreply.github.com>
2023-09-29 16:54:14 +0100

    remove session

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,5 +16,11 @@ def states():
     return render_template('7-states_list.html', **states_list)
 
 
+@app.teardown_appcontext
+def teardown(exc):
+    """ Remove the current SQLAlchemy session """
+    storage.close()
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
